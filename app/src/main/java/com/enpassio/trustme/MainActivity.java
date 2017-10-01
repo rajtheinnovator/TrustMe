a88dc0fd4ec2d583102f9ab8db6ea33cfab01c6d
ABHISHEK RAJ <rajtheinnovatorabhishek@gmail.com>
2017-10-01 06:00:06 +0530

    Added runtime permission for location

--- app/src/main/java/com/enpassio/trustme/MainActivity.java
+++ app/src/main/java/com/enpassio/trustme/MainActivity.java
@@ -1,13 +1,73 @@
 package com.enpassio.trustme;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final int MULTIPLE_PERMISSIONS = 10;
+    String[] permissions = new String[]{
+            Manifest.permission.ACCESS_COARSE_LOCATION,
+            Manifest.permission.ACCESS_FINE_LOCATION};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getUsersLocation();
+    }
+
+    private void getUsersLocation() {
+        if (checkPermissions()) {
+            //  permissions  granted.
+        }
+    }
+
+    private boolean checkPermissions() {
+        int result;
+        List<String> listPermissionsNeeded = new ArrayList<>();
+        for (String p : permissions) {
+            result = ContextCompat.checkSelfPermission(this, p);
+            if (result != PackageManager.PERMISSION_GRANTED) {
+                listPermissionsNeeded.add(p);
+            }
+        }
+        if (!listPermissionsNeeded.isEmpty()) {
+            ActivityCompat.requestPermissions(this,
+                    listPermissionsNeeded.toArray(new String[listPermissionsNeeded.size()]),
+                    MULTIPLE_PERMISSIONS);
+            return false;
+        }
+        return true;
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[],
+                                           int[] grantResults) {
+        switch (requestCode) {
+            case MULTIPLE_PERMISSIONS: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // permissions granted.
+                } else {
+                    String permission = "";
+                    for (String per : permissions) {
+                        permission += "\n" + per;
+                    }
+                    Log.i("TAG", "permission is: " + permission);
+                    // permissions list of not granted permission
+                }
+                return;
+            }
+        }
     }
 }
